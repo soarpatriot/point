a8e04c23d6494e8c4c745d462ff5a3fb2ecbc576
Soar <85624529@qq.com>
2015-12-13 12:42:07 +0800

    add router

--- src/api.com/models/area.go
+++ src/api.com/models/area.go
@@ -1,6 +1,11 @@
 package models
 
 type Area struct {
-	Id     int
-	Points []*Point
+	Id        int
+	Latitude  float64
+	Longitude float64
+	Label     string
+	Code      string
+	Price     string
+	Points    []*Point
 }
--- src/api.com/models/point.go
+++ src/api.com/models/point.go
@@ -1,6 +1,7 @@
 package models
 
 type Point struct {
+	Id  int
 	Lat float64
 	Lng float64
 }
